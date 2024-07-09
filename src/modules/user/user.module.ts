0bc72ee54dabea671cf8985a2454ec982413b895
Jinyuan Zhang <jinyuanzhang1992@hotmail.com>
2024-07-09 10:20:38 +0930

    summary:nestjs

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -2,10 +2,8 @@ import { Module } from '@nestjs/common';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
 import { UserResolver } from './user.resolver';
-import { CommonModule } from '../../common/common.module';
 
 @Module({
-  imports: [CommonModule],
   controllers: [UserController],
   providers: [UserService, UserResolver],
 })
